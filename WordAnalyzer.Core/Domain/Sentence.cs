023a4943079399ed14a089ba1985591ea7e862bd
arcaim.interview@outlook.com <arcaim.neti@outlook.com>
2018-07-26 10:17:13 +0200

    Simple refactoring

--- WordAnalyzer.Core/Domain/Sentence.cs
+++ WordAnalyzer.Core/Domain/Sentence.cs
@@ -4,11 +4,6 @@ namespace WordAnalyzer.Core.Domain
 {
     public class Sentence
     {
-    //     public string this [int index] // sam wymyśliłem, pewnie brak zastosowania
-    //     {
-    //         get { return Words; }
-    //     }
-
         public IEnumerable<string> Words { get; set; }
 
         public Sentence(IEnumerable<string> words)
