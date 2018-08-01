02ae1872ca35a17628e116a0de858fe46093047b
arcaim.interview@outlook.com <arcaim.neti@outlook.com>
2018-08-01 06:44:47 +0200

    Code cleaning

--- WordAnalyzer.Infrastructure/Commands/LoadText.cs
+++ WordAnalyzer.Infrastructure/Commands/LoadText.cs
@@ -2,10 +2,6 @@ namespace WordAnalyzer.Infrastructure.Commands
 {
     public class LoadText
     {
-        // public LoadText()
-        // {
-            
-        // }
         public string Text { get; set; }
     }
 }
